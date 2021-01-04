c278732afda33892dc897bb3cf90d0e49061ee08
Chukwu Matthew <matthewnfvcb@gmail.com>
2021-01-04 14:12:53 +0100

    netlify functions added

--- pages/contact-form/index.vue
+++ pages/contact-form/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-gray-400">
+  <div>
     <!-- <div
       
       class="mt-8 bg-green-500 py-8 px-4 text-center text-xl font-semibold text-gray-200 tracking-widest"
@@ -7,85 +7,56 @@
       Your message has been sent successfully and will be attended to
       accordingly.
     </div> -->
-    <form @submit.prevent="sendMsg" class="grid grid-cols-1 gap-y-6 py-8">
-      <div class="px-4">
-        <label for="name">Full name</label>
-        <div class="relative rounded-lg shadow-sm">
-          <input
-            v-model="form.name"
-            required
-            name="name"
-            id="name"
-            class="block w-full py-3 px-4 transition ease-in-out duration-150"
-            placeholder="Name"
-          />
-        </div>
-      </div>
-      <div class="px-4">
-        <label for="email" class="sr-only">Email</label>
-        <div class="relative rounded-md shadow-sm">
-          <input
-            v-model="form.email"
-            required
-            name="emaiL"
-            id="email"
-            type="email"
-            class="block w-full py-3 px-4 transition ease-in-out duration-150"
-            placeholder="Email"
-          />
-        </div>
-      </div>
-      <!-- <div class="px-4">
-        <label for="phone" class="sr-only">*Phone</label>
-        <div class="relative rounded-md shadow-sm">
-          <input
-            v-model="form.phone"
-            required
-            name="phone"
-            id="phone"
-            class="form-input block w-full py-3 px-4 transition ease-in-out duration-150"
-            placeholder="Phone No."
-          />
-        </div>
-      </div> -->
-      <!-- <div class="px-4">
-        <label for="staffNo" class="sr-only">*StaffNo</label>
-        <div class="relative rounded-md shadow-sm">
-          <input
-            v-model="staffNo"
-            required
-            name="staffNo"
-            id="staffNo"
-            class="block w-full py-3 px-4 transition ease-in-out duration-150"
-            placeholder="Staff No."
-          />
-        </div>
-      </div> -->
-      <div class="px-4">
-        <label for="message" class="sr-only">Message</label>
-        <div class="relative rounded-md shadow-sm">
-          <textarea
-            required
-            v-model="form.message"
-            name="message"
-            id="message"
-            rows="4"
-            class="form-input block w-full py-3 px-4 placeholder-gray-500 transition ease-in-out duration-150"
-            placeholder="Message"
-          ></textarea>
-        </div>
-      </div>
-      <div class="p-4">
-        <span class="inline-flex rounded-md shadow-sm">
+    <div class="mt-8">
+      <p class="px-4">Contact Form</p>
+      <div>
+        <form @submit.prevent="sendMsg" class="px-4 py-6 space-y-4">
+          <div class="">
+            <label for="name">Name</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              v-model="name"
+              placeholder="Enter name"
+              aria-describedby="name"
+              class="block border-2 rounded-md w-full p-2 text-gray-700 bg-gray-100 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner"
+            />
+          </div>
+          <div>
+            <label for="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              v-model="email"
+              placeholder="Enter email"
+              aria-describedby="email"
+              class="block border-2 rounded-md w-full p-2 text-gray-700 bg-gray-100 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner"
+            />
+          </div>
+          <div>
+            <label for="message">Message</label>
+            <textarea
+              name="message"
+              id="message"
+              v-model="message"
+              type="text"
+              rows="4"
+              aria-describedby="message"
+              class="block border-2 w-full rounded-md p-2 text-gray-700 bg-gray-100 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner"
+            />
+          </div>
+          <br />
           <button
-            type="submit"
-            class="inline-flex justify-center py-3 px-6 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
+            type="sumbit"
+            class="block w-full rounded-md text-gray-300 font-bold text-xl p-3 text-center bg-gray-700 tracking-widest uppercase"
           >
-            {{ loading ? "Sending Message..." : "Submit" }}
+            {{ loading ? "Please wait.." : "Send" }}
           </button>
-        </span>
+        </form>
       </div>
-    </form>
+    </div>
   </div>
 </template>
 
@@ -95,43 +66,31 @@ export default {
   data() {
     return {
       loading: false,
-      form: {
-        name: "",
-        email: "",
-        //   staffNo: "",
-        // phone: "",
-        message: "",
-      },
+      name: "",
+      email: "",
+      //   staffNo: "",
+      // phone: "",
+      message: "",
     };
   },
   methods: {
-    async sendMsg() {
+     async sendMsg() {
       this.loading = true;
-      try {
-        let uri = "https://payslipvault.netlify.app";
-        const response = await this.$axios.$post(
-          uri,
-          "/.netlify/functions/contact-mail",
-          {
-            name: this.form.name,
-            email: this.form.email,
-            // phone: this.form.phone,
-            message: this.form.message,
-          }
-        );
-        console.log(form.name);
-        console.log(form.email);
-        console.log(form.message);
-        this.loading = false;
-        this.$toast.success(response);
-        this.form.name = "";
-        this.form.email = "";
-        // this.form.phone = '';
-        this.form.message = "";
-      } catch (error) {
-        this.loading = false;
-        this.$toast.error(error);
-      }
+     try {
+       const response = await this.$axios.$post('/.netlify/functions/contact-mail', {
+         name: this.name,
+         email: this.email,
+         message: this.message,
+       })
+       this.$toast.success('Mail Sent');
+       this.loading = false;
+       this.name = ''
+       this.email = ''
+       this.message = ''
+     } catch(error) {
+       this.loading = false;
+       this.$toast.error(error);
+     }     
     },
   },
 };
